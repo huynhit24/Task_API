1d74fc9028e6c147206d4b2ee8707eeaa870b9e7
huynhit24 <huynh.it.24@gmail.com>
2022-12-21 14:03:04 +0700

    ok model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+    },
+    profile: {
+        type: String,
+    },
+    gender: {
+        type: Boolean,
+    },   
+    avatar: {
+        data: Buffer,
+        contentType: String,
+    },
+    isActived: {
+        type: Boolean
+    },  
+    isAdmin: {
+        type: Boolean
+    }, 
+    status: {
+        type: Boolean
+    },   
+});
+
+
+module.exports = mongoose.model('User', userSchema);
